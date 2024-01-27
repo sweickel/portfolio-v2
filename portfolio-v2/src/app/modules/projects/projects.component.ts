37f581abe4d243a9861f4e5cf5c51bac20ddce19
Sean Weickel <sweickel@gmail.com>
2024-01-26 21:27:09 -0500

    new project assets and updated content

--- portfolio-v2/src/app/modules/projects/projects.component.ts
+++ portfolio-v2/src/app/modules/projects/projects.component.ts
@@ -6,15 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
+  githubBaseUrl = 'https://sweickel.github.io'
   projects = [
     {
       imgSrc: '../../assets/Calculator.png',
-      url: 'https://sweickel.github.io/calculator/',
+      url: `${this.githubBaseUrl}/calculator/`,
       title: 'Calculator',
       body: 'Simple Calculator built with Javascript.',
     },
-    {},
-    {},
-    {},
+    {
+      imgSrc: '../../assets/scissors.png',
+      url: `${this.githubBaseUrl}/rock-paper-scissors/`,
+      title: 'Rock, Paper, Scissors',
+      body: 'This is a fun Rock, Paper, Scissors browser console game.'
+    },
+    {
+      imgSrc: '../../assets/etch-a-sketch.png',
+      url: `${this.githubBaseUrl}/etch-a-sketch`,
+      title: 'Make-a-Sketch',
+      body: 'HTML/CSS/Javascript implementation of the kids\' toy Etch-a-Sketch.'
+    },
+    {
+      imgSrc: '../../assets/tic-tac-toe.png',
+      url: `${this.githubBaseUrl}/tic-tac-toe`,
+      title: 'Tic-Tac-Toe',
+      body: 'HTML/CSS/Javascript implementation of Tic-Tac-Toe.'
+    },
+    {
+      imgSrc: '../../assets/restaurant.png',
+      url: `${this.githubBaseUrl}/restaurant`,
+      title: 'Restaurant website satire',
+      body: 'Goofy restaurant website satire.'
+    },
   ]
+
+
+
 }
