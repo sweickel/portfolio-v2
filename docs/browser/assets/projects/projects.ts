7a0bfcb5a3b6ede404ba93d94cd9f7ab681dc70a
Sean Weickel <sweickel@gmail.com>
2024-08-04 12:24:19 -0400

    attach prod build

---
+++ docs/browser/assets/projects/projects.ts
@@ -0,0 +1,42 @@
+export const githubBaseUrl = 'https://sweickel.github.io';
+export interface IProject {
+  imgSrc: string;
+  url: string;
+  title: string;
+  body: string;
+}
+
+export function projects():IProject[] { 
+  return [
+    {
+      imgSrc: '../../assets/images/Calculator.png',
+      url: `${githubBaseUrl}/calculator/`,
+      title: 'Calculator',
+      body: 'Simple Calculator built with Javascript.',
+    },
+    {
+      imgSrc: '../../assets/images/scissors.png',
+      url: `${githubBaseUrl}/rock-paper-scissors/`,
+      title: 'Rock, Paper, Scissors',
+      body: 'This is a fun Rock, Paper, Scissors browser console game.'
+    },
+    {
+      imgSrc: '../../assets/images/etch-a-sketch.png',
+      url: `${githubBaseUrl}/etch-a-sketch`,
+      title: 'Make-a-Sketch',
+      body: 'HTML/CSS/Javascript implementation of the kids\' toy Etch-a-Sketch.'
+    },
+    {
+      imgSrc: '../../assets/images/tic-tac-toe.png',
+      url: `${githubBaseUrl}/tic-tac-toe`,
+      title: 'Tic-Tac-Toe',
+      body: 'HTML/CSS/Javascript implementation of Tic-Tac-Toe.'
+    },
+    {
+      imgSrc: '../../assets/images/restaurant.png',
+      url: `${githubBaseUrl}/restaurant`,
+      title: 'Restaurant website satire',
+      body: 'Goofy restaurant website satire.'
+    },
+  ]
+};
