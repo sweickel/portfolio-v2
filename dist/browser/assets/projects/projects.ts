e8e2a7e35bc705d3dc873c8394c18332c02ee7cf
Sean Weickel <sweickel@gmail.com>
2024-08-14 23:47:09 -0400

    pathing updates

--- dist/browser/assets/projects/projects.ts
+++ dist/browser/assets/projects/projects.ts
@@ -9,31 +9,31 @@ export interface IProject {
 export function projects():IProject[] { 
   return [
     {
-      imgSrc: '../../assets/images/Calculator.png',
+      imgSrc: 'assets/images/Calculator.png',
       url: `${githubBaseUrl}/calculator/`,
       title: 'Calculator',
       body: 'Simple Calculator built with Javascript.',
     },
     {
-      imgSrc: '../../assets/images/scissors.png',
+      imgSrc: 'assets/images/scissors.png',
       url: `${githubBaseUrl}/rock-paper-scissors/`,
       title: 'Rock, Paper, Scissors',
       body: 'This is a fun Rock, Paper, Scissors browser console game.'
     },
     {
-      imgSrc: '../../assets/images/etch-a-sketch.png',
+      imgSrc: 'assets/images/etch-a-sketch.png',
       url: `${githubBaseUrl}/etch-a-sketch`,
       title: 'Make-a-Sketch',
       body: 'HTML/CSS/Javascript implementation of the kids\' toy Etch-a-Sketch.'
     },
     {
-      imgSrc: '../../assets/images/tic-tac-toe.png',
+      imgSrc: 'assets/images/tic-tac-toe.png',
       url: `${githubBaseUrl}/tic-tac-toe`,
       title: 'Tic-Tac-Toe',
       body: 'HTML/CSS/Javascript implementation of Tic-Tac-Toe.'
     },
     {
-      imgSrc: '../../assets/images/restaurant.png',
+      imgSrc: 'assets/images/restaurant.png',
       url: `${githubBaseUrl}/restaurant`,
       title: 'Restaurant website satire',
       body: 'Goofy restaurant website satire.'
